850a613babe24ed6e6556fc7207a64fdeec3e36b
Vivek Poddar <vivekimsit@gmail.com>
2016-08-16 21:09:50 +0530

    Serve index.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.use(express.static(__dirname));
 app.use(serve);
 
 function serve(req, res, next) {
-	if (req.accepts('html')) res.sendFile(__dirname + '/index.html');
-	next();
+	if (req.accepts('html')) res.sendFile(__dirname + '/delivery/web/index.html');
+	else next();
 }
 
 const PORT = 3000;
