4975a93ad06c4834b8180ded52cd8da9de77fa2c
Vivek Poddar <vivekimsit@gmail.com>
2016-08-18 22:56:06 +0530

    Server quote by using angular $http.

--- delivery/web/js/main.js
+++ delivery/web/js/main.js
@@ -1,3 +1,17 @@
-function AppCtrl($scope) {
-	$scope.title = 'Hello world';
+function AppCtrl($http) {
+	this.message = 'Baking your awesome quote';
+
+	$http({
+		method: 'GET',
+		url: '/api/v1',
+		headers: {
+			'Accept': 'application/json'
+		}
+	}).then(function successCallback(response) {
+		this.quote = response.data;
+		this.message = '';
+		console.log(response);
+	}.bind(this), function errorCallback(response) {
+		console.log(response);
+	});
 }
